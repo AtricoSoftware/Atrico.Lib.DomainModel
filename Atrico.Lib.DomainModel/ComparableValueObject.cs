f1ead7f5178608693b3552e3cebd40dc5a0ef3ea
AtricoSoftware <robrimmer@atrico.net>
2015-04-16 12:57:31 +0100

    Added ComparableValueObject

---
+++ Atrico.Lib.DomainModel/ComparableValueObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Atrico.Lib.DomainModel
+{
+    public abstract class ComparableValueObject<T> : ValueObject<T>, IComparable, IComparable<T> where T : ValueObject<T>
+    {
+        public int CompareTo(object obj)
+        {
+            return CompareTo(obj as T);
+        }
+
+        public int CompareTo(T other)
+        {
+            // Null is smallest
+            if (ReferenceEquals(other, null)) return -1;
+            // If types are different, compare by type
+            var comp = String.Compare(GetType().FullName, other.GetType().FullName, StringComparison.Ordinal);
+            return comp != 0 ? comp : CompareToImpl(other);
+        }
+
+        protected abstract int CompareToImpl(T other);
+    }
+}
